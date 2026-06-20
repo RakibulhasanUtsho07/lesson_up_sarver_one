cb579fa064d95526f8fe0b2c7578aa8fc839d5ca
rakibulhasanutsho <162662167+rakibulhasanutsho@users.noreply.github.com>
2026-06-20 09:28:46 +0600

    complete lesson post and lessons get mathod

---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = 5000;
+
+// Middleware & Configuration
+app.use(cors());
+app.use(express.json());
+dotenv.config();
+
+const uri = process.env.MONGO_DB_URI;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+async function run() {
+  try {
+  
+    await client.connect();
+    
+    const database = client.db("lessons_up");
+    const lessonsCollection = database.collection("lessons");
+
+  
+    app.post("/lessons", async (req, res) => {
+      try {
+        const lessons = req.body;
+        console.log(lessons, "lessons data received");
+
+        
+        const result = await lessonsCollection.insertOne(lessons);
+        res.status(201).json(result);
+      } catch (error) {
+        console.error("Error inserting lesson:", error);
+        res.status(500).send({ message: "Internal Server Error" });
+      }
+    });
+    app.get("/lessons", async(req, res)=>{
+      try{
+        const result = await lessonsCollection.find().toArray()
+      res.send(result)
+      }catch(error){
+        console.error(error)
+        res.status(500).send({message:  "Server error occurred"})
+      }
+    })
+  
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+  } catch (error) {
+    console.error("Database connection error:", error);
+  }
+
+}
+
+// run ফাংশন কল করা
+run().catch(console.dir);
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
